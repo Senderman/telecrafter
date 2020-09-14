aef74ec6a7d835f64bbb780957b9decc344251b6
Fyodor Doletov <doletov.fyodor@yandex.ru>
2020-09-14 21:46:13 +0300

    add jar check

--- src/main/java/com/senderman/telecrafter/telegram/command/InstallPlugin.java
+++ src/main/java/com/senderman/telecrafter/telegram/command/InstallPlugin.java
@@ -42,6 +42,10 @@ public class InstallPlugin implements CommandExecutor {
         }
 
         Document telegramDoc = message.getReply().getDocument();
+        if (!telegramDoc.isJar()){
+            telegram.sendMessage(chatId, "Это не файл плагина, а Х какая то!");
+            return;
+        }
         File pluginFile;
         try {
             pluginFile = downloadDocument(telegramDoc);
